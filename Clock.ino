1b5da52555cba72c70fde3134a76a1baad71987a
Jiun-You Tzeng <joe5343281@gmail.com>
2017-12-24 18:51:55 +0800

    Add files via upload

--- Clock.ino
+++ Clock.ino
@@ -7,7 +7,8 @@ void digitalWriteRange(const int16_t, const int16_t, const int16_t);
 void timeUpdate();
 void buttonRisingTriggered(int16_t);
 
-const int8_t buttonPin = 2;
+const int8_t redButtonPin = 2;
+const int8_t whiteButtonPin = 9;
 const int8_t D0 = 3; 
 const int8_t D1 = 4;
 const int8_t D2 = 5;
@@ -19,8 +20,8 @@ const int8_t dataPin = 8;
 const int8_t clockPin = 12;
 const int8_t latchPin = 13;
 
-int8_t hours = 13;
-int8_t minutes = 59;
+int8_t hours = 20;
+int8_t minutes = 17;
 int8_t seconds = 0;
 
 int8_t diosMin;
@@ -32,14 +33,18 @@ int8_t * times[4] = {&diosMin, &ditsMin, &dioshour, &ditshour};
 /*                          *\
        R&W Button State
 \*                          */
-int8_t lastButtonState = LOW;
-int8_t buttonState;
+int8_t lastRedButtonState = LOW;
+int8_t redButtonState;
 int8_t ledState = LOW;
+
+int8_t lastWhiteButtonState = LOW;
+int8_t whiteButtonState;
 
 /*                               *\
    R&W Button Triggered Interval
 \*                               */
-uint32_t lastDebounceTime = 0;
+uint32_t lastRedDebounceTime = 0;
+uint32_t lastWhiteDebounceTime = 0;
 uint32_t debounceDelay = 25;
 
 uint8_t count = 0;
@@ -75,8 +80,25 @@ const int seven_seg_digits[10] =  {
                                     B1110011   // = 9
                                   };
 
+// Note frequency
+#define Do  261 // 1
+#define Re  293 // 2
+#define Me  329 // 3
+#define Fa  349 // 4
+#define Sol 392 // 5
+#define La  440 // 6
+#define Si  493 // 7
+
+
+const int16_t christmasSong[] = {Me, Me, Me, 0, Me, Me, Me, 0, Me, Sol, Do, Re, Me, 0,
+                                 Fa, Fa, Fa, Fa, Fa, Me, Me, Me, Me, Re, Re, Me, Re, 0, Sol, 0,
+                                 Me, Me, Me, 0, Me, Me, Me, 0, Me, Sol, Do, Re, Me, 0,
+                                 Fa, Fa, Fa, Fa, Fa, Me, Me, Me, Sol, Sol, Fa, Re, Do
+                                 
+};
+
 void setup() {
-  pinMode(buttonPin, INPUT);
+  pinMode(redButtonPin, INPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
@@ -99,9 +121,10 @@ void loop() {
       int16_t oriData = Serial.read();
       int16_t handledData = oriData - '0';
       if(handledData >= 0 && handledData <= 9){
-      space[digits] =  handledData;
+      space[digits] =  handledData;  
       digits++;
       }
+      Serial.println(handledData);
     }
     hourSum = space[0]*10 + space[1];
     minuteSum = space[2]*10 + space[3];
@@ -112,33 +135,23 @@ void loop() {
     } 
   }
   
-  if(millis() - lastInterval >= 1000){
-    // Time
-    lastInterval = millis();
-
-    diosMin = minutes % 10; // Digit in ones of Minutes
-    ditsMin = minutes / 10; // Digit in tens of Minutes
-    dioshour = hours % 10; // Digit in ones of Hours
-    ditshour = hours / 10; // Digit in tens of Hours
-       
-    seconds++;
-    if(seconds>=60) timeUpdate();
-  }
+  addOneSencond();
 
   // buttonState
-  int16_t reading = digitalRead(buttonPin);
-  
-  buttonRisingTriggered(reading);
+  int16_t redReading = digitalRead(redButtonPin);
+  int16_t whiteReading = digitalRead(whiteButtonPin);
+
+  buttonRisingTriggered(redReading, redButtonPin, &redButtonState, &lastRedButtonState, &lastRedDebounceTime);
+  buttonRisingTriggered(whiteReading, whiteButtonPin, &whiteButtonState, &lastWhiteButtonState, &lastWhiteDebounceTime);
+  //buttonRisingTriggered(reading);
   
   // When ledState is LOW, D0~D3 will be enabled.
   digitalWriteRange(D0, D3, ledState);
   
-  if(ledState == LOW) enableDx((count % 4) + pinOffset, *times[count]);
-  count++;
-  count %= 4;
-  delay(5);
+  showTime(ledState);
   
-  lastButtonState = reading;
+  lastRedButtonState = redReading;
+  lastWhiteButtonState = whiteReading;
 }
 
 void enableDx(const int8_t Dx, int8_t number){
@@ -151,13 +164,11 @@ void enableDx(const int8_t Dx, int8_t number){
   shiftOut(dataPin, clockPin, LSBFIRST, seven_seg_digits[number]);
   digitalWrite(latchPin, HIGH);
 }
-
-void pinModeRange(const int16_t pinS, const int16_t pinE, const int16_t mode){
-  for(int i=pinS; i<=pinE; i++) pinMode(i, mode);
-}
-
-void digitalWriteRange(const int16_t pinS, const int16_t pinE, const int16_t state){
-  for(int i=pinS; i<=pinE; i++) digitalWrite(i, state);
+void showTime(int8_t ledState){
+  if(ledState == LOW) enableDx((count % 4) + pinOffset, *times[count]);
+  count++;
+  count %= 4;
+  delay(5);
 }
 
 void timeUpdate(){
@@ -176,19 +187,61 @@ void timeUpdate(){
   }
 }
 
-void buttonRisingTriggered(int16_t reading){
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
+void computeTime(){
+  diosMin = minutes % 10; // Digit in ones of Minutes
+  ditsMin = minutes / 10; // Digit in tens of Minutes
+  dioshour = hours % 10; // Digit in ones of Hours
+  ditshour = hours / 10; // Digit in tens of Hours
+}
+
+void addOneSencond(){
+  if(millis() - lastInterval >= 1000){
+    // Time
+    lastInterval = millis();
+
+    seconds++;
+    if(seconds>=60) timeUpdate();
+    computeTime();   
+  }
+}
+
+void buttonRisingTriggered(int16_t reading, const int8_t buttonPin, int8_t *buttonState, int8_t *lastButtonState,  int32_t *lastDebounceTime){
+  if (reading != *lastButtonState) {
+    *lastDebounceTime = millis();
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      if (buttonState == HIGH) {
-        ledState = !ledState;
+  if ((millis() - *lastDebounceTime) > debounceDelay) {
+    if (reading != *buttonState) {
+      *buttonState = reading;
+      if(buttonPin == redButtonPin){
+        if(*buttonState == HIGH) ledState = !ledState;
+      }
+      else{
+        if(*buttonState == HIGH) {
+          playMusic();
+          
+        }
       }
     }
+  }
+}
+
+void pinModeRange(const int16_t pinS, const int16_t pinE, const int16_t mode){
+  for(int i=pinS; i<=pinE; i++) pinMode(i, mode);
+}
+
+void digitalWriteRange(const int16_t pinS, const int16_t pinE, const int16_t state){
+  for(int i=pinS; i<=pinE; i++) digitalWrite(i, state);
+}
+
+void playMusic(){
+  int16_t len = (sizeof christmasSong) / sizeof(int16_t);
+  for(int16_t i=0; i<len; i++){
+    tone(buzzerPin, christmasSong[i], 180);
+    delay(180);
+    addOneSencond();
+    showTime(ledState);
+    Serial.println(seconds);
   }
 }
 
